5265b1c626b85feceaf860b3b9400aaaafa0e22c
Juan Carlos <JuanCarlospaco@gmail.com>
2018-08-15 16:16:29 -0300

    Add keycode to key and key to keycode JSONs for conversions

--- src/xdo.nim
+++ src/xdo.nim
@@ -1,6 +1,43 @@
-import osproc, strformat, strutils, terminal, random
+import osproc, strformat, strutils, terminal, random, json
 
 const version* = staticExec("xdo -v")  ## XDo Version (SemVer).
+
+let
+  keycode2char* = %* {
+    "8":"backspace","9":"tab","13":"enter","16":"shift","17":"ctrl","18":"alt",
+    "19":"pause/break","20":"caps lock","27":"esc","32":"space","33":"page up",
+    "34":"page down","35":"end","36":"home","37":"left","38":"up","39":"right",
+    "40":"down","45":"insert","46":"delete","48":"0","49":"1","50":"2","51":"3",
+    "52":"4","53":"5","54":"6","55":"7","56":"8","57":"9","65":"a","66":"b",
+    "67":"c","68":"d","69":"e","70":"f","71":"g","72":"h","73":"i","74":"j",
+    "75":"k","76":"l","77":"m","78":"n","79":"o","80":"p","81":"q","82":"r",
+    "83":"s","84":"t","85":"u","86":"v","87":"w","88":"x","89":"y","90":"z","91":"windows",
+    "93":"right click","96":"numpad 0","97":"numpad 1","98":"numpad 2",
+    "99":"numpad 3","100":"numpad 4","101":"numpad 5","102":"numpad 6",
+    "103":"numpad 7","104":"numpad 8","105":"numpad 9","106":"numpad *",
+    "107":"numpad +","109":"numpad -","110":"numpad .","111":"numpad /",
+    "112":"f1","113":"f2","114":"f3","115":"f4","116":"f5","117":"f6",
+    "118":"f7","119":"f8","120":"f9","121":"f10","122":"f11","123":"f12",
+    "144":"num lock","145":"scroll lock","182":"my computer",
+    "183":"my calculator","186":";","187":"=","188":",","189":"-","190":".",
+    "191":"/","192":"`","219":"[","220":"\\","221":"]","222":"'"
+  }  ## Statically compiled JSON that maps KeyCodes integers Versus Keys strings.
+  char2keycode* = %* {
+    "backspace":8,"tab":9,"enter":13,"shift":16,"ctrl":17,"alt":18,
+    "pause/break":19,"caps lock":20,"esc":27,"space":32,"page up":33,
+    "page down":34,"end":35,"home":36,"left":37,"up":38,"right":39,"down":40,
+    "insert":45,"delete":46,"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,
+    "7":55,"8":56,"9":57,"a":65,"b":66,"c":67,"d":68,"e":69,"f":70,"g":71,
+    "h":72,"i":73,"j":74,"k":75,"l":76,"m":77,"n":78,"o":79,"p":80,"q":81,
+    "r":82,"s":83,"t":84,"u":85,"v":86,"w":87,"x":88,"y":89,"z":90,"windows":91,
+    "right click":93,"numpad 0":96,"numpad 1":97,"numpad 2":98,"numpad 3":99,
+    "numpad 4":100,"numpad 5":101,"numpad 6":102,"numpad 7":103,"numpad 8":104,
+    "numpad 9":105,"numpad *":106,"numpad +":107,"numpad -":109,"numpad .":110,
+    "numpad /":111,"f1":112,"f2":113,"f3":114,"f4":115,"f5":116,"f6":117,
+    "f7":118,"f8":119,"f9":120,"f10":121,"f11":122,"f12":123,"num lock":144,
+    "scroll lock":145,"my computer":182,"my calculator":183,";":186,"=":187,
+    ",":188,"-":189,".":190,"/":191,"`":192,"[":219,"\\":220,"]":221,"'":222
+  }  ## Statically compiled JSON that maps Keys strings Versus KeyCodes integers.
 
 type
   Actions* {.pure.} = enum             ## All Actions.
@@ -101,15 +138,24 @@ proc xdo_move_mouse_left_100px*(repetitions: int8): tuple =
   for i in 0..repetitions:
     execCmdEx("xdo pointer_motion -x +100")
 
+proc xdo_get_pid*(): int =
+  ## Get PID of a window, integer type.
+  parseInt(execCmdEx("xdo pid").output.strip)
+
+proc xdo_get_id*(): int =
+  ## Get ID of a window, integer type.
+  parseInt(execCmdEx("xdo id").output.strip)
+
+proc xdo_mouse_move_alternating*(move: tuple[x: int, y: int], repetitions: int8): tuple =
+  ## Move mouse alternating to Left/Right Up/Down, AKA Zig-Zag movements.
+  var xx, yy: string
+  for i in 0..repetitions:
+    xx = if i mod 2 == 0: "+" else: "-" & $move.x
+    yy = if i mod 2 == 0: "+" else: "-" & $move.y
+    execCmdEx(fmt"xdo pointer_motion -x {xx} -y {yy}")
+
+
 discard """
-get_pid()
-
-get_id()
-
-proc xdo_mouse_move_alternating*(): tuple =
-  ## Move mouse alternating to Left and Right, AKA ZigZag move.
-  execCmdEx(fmt"xdo move -x {maxx.rand} -y {maxy.rand} -p {pid}")
-
 proc xdo_mouse_left_click*(): tuple =
   ## Mouse Left Click, 1 repetitions.
   execCmdEx(fmt"xdo move -x {maxx.rand} -y {maxy.rand} -p {pid}")
