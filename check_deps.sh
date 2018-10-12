01e0a652006fbfdb7da819e03a64b6edc1a58aa1
juancarlospaco <juancarlospaco@gmail.com>
2018-10-12 19:24:39 -0300

    Move to the Temp folder before downloading things

--- check_deps.sh
+++ check_deps.sh
@@ -13,10 +13,11 @@ else
 
     if [[ $input != "n" ]]; then
       echo "xdo is installing..."
+      cd /tmp  # Move to the Temp folder.
       git clone -q https://github.com/baskerville/xdo # clone te repo
       cd xdo # go into dir
-      make > /dev/null
-      sudo make install > /dev/null # sudo will prompt user for password
+      make              # > /dev/null # If you dont want to see the info uncomment
+      sudo make install # > /dev/null # sudo will prompt user for password
 
       cd ..; rm -rf xdo # cleanup
       echo "Done! xdo should now be installed."
